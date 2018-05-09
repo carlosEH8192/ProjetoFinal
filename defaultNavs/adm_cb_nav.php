0c27a546725c8e6f5b25b1bf3b9efd9bd98e8e33
Carlos Eduardo <carloseh.355@gmail.com>
2018-05-09 18:34:25 -0300

    Alteracoes em DefaultNavs: adm_cb_nav.php -> nova variavel de redirecionamento para Gestao de Turmas; adm_cb_nav.html -> reajustado para utilizar a nova variavel.

--- defaultNavs/adm_cb_nav.php
+++ defaultNavs/adm_cb_nav.php
@@ -7,8 +7,9 @@
     $gestao_adms = ROOT."/adm/adms/index.php";
     $gestao_alunos = ROOT."/adm/alunos/index.php";
     $gestao_cursos = ROOT."/adm/cursos/index.php";
-    $gestao_professores = ROOT."/adm/professores/index.php";
     $gestao_nomes_turma = ROOT."/adm/nomesTurma/index.php";
+    $gestao_professores = ROOT."/adm/professores/index.php";
+    $gestao_turmas = ROOT."/adm/turmas/index.php";
 
     include_once("adm_cb_nav.html");
 ?>
