76c0a304d082650a87cad3a0b5e75a7eb74e2955
Carlos Eduardo <carloseh.355@gmail.com>
2018-05-12 01:52:01 -0300

    Detalhes ajustados nos Snippets: conecta.php, resultado.php e array_fetch_assoc.php

--- deus/snippet/conecta.php
+++ deus/snippet/conecta.php
@@ -1,3 +1,6 @@
 <?php // => /deus/snippet/conecta.php
-	$conexao = mysqli_connect("localhost", "root", "", "alcidesmaya_tech");
+    $conexao = mysqli_connect(
+        "mysql101.prv.f1.k8.com.br", "alcidesmaya1",
+        "ProjetoAlcidesMaya", "alcidesmaya1"
+    );
 ?>
